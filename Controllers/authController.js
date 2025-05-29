bbc92e3827e58499f784586b27a4bccbc864fad1
Vengat-P <cronyvengat33@gmail.com>
2025-05-29 14:10:07 +0530

    updated

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -76,8 +76,8 @@ export const forgotPassword = async (req, res) => {
       user.email,
       "Password Reset Link",
       `you are receiving this because you try to reset your password for you acount.
-      Click the following Link to complete the process https://passwordresetflow123.netlify.app/reset-password/${user._id}/${token} 
-      
+      Click the following Link to complete the process 
+      https://passwordauthreset.netlify.app/reset-password/${user._id}/${token} 
       please ignore if you have not Requested for reset password`
     );
     
